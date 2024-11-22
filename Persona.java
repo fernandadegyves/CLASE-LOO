d9eb2a5777c05831b93ed58da3a388d53079cfbb
Marlene Sánchez <152517085+Marlene-Sanchez@users.noreply.github.com>
2024-11-22 11:54:23 -0600

    Update Persona.java
    Creation of method .showInfo()

--- Persona.java
+++ Persona.java
@@ -1,1 +1,18 @@
-
+public class Persona{
+  public String name;
+  public int age;
+  public Persona(){
+    this.name="";
+    this.age=0;
+  }
+  public Persona(String name, int age){
+    this.name=name;
+    this.age=age;
+    
+  }
+  public void showInfo(){
+    System.out.println("Nombre: "+this.name);
+    System.out.println("Edad: "+this.age);
+  }
+  
+}
